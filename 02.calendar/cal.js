6efffe41050b432b36b5c1c6df3d4f6adcd72bde
triton27 <na54heart@gmail.com>
2024-08-10 23:13:35 +0900

    refactor: 変数名の変更、メソッドの切り分けなど

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,28 +1,44 @@
 var argv = require('minimist')(process.argv.slice(2));
 const now = new Date();
-const year = argv['y'] == null ? now.getFullYear() : (argv['y'])
-const month = argv['m'] == null ? now.getMonth() + 1 : (argv['m'])
-// 対象年月の1日目の曜日を取得
-let dayOfWeek = new Date(year, month - 1, 1).getDay();
-// 対象年月の最終日を取得
-const days = new Date(year, month, 0).getDate();
+const year = argv['y'] || now.getFullYear();
+const month = argv['m'] || now.getMonth() + 1;
 
-const week = ['日', '月', '火', '水', '木', '金', '土']
 const SPACE = ' ';
 
-console.log(`${month}月 ${year}`.padStart(13, SPACE));
-console.log(week.join(SPACE));
-
-// 初日のスペース出力
-process.stdout.write(`${SPACE.repeat(3).repeat(dayOfWeek)}`);
-
-for (let day = 1; day <= days; day++) {
-    let dayString = day.toString().padStart(2, SPACE) + SPACE;
-        if (dayOfWeek == 6) {
-            console.log(dayString);
-            dayOfWeek = 0;
-        } else {
-            process.stdout.write(dayString);
-            dayOfWeek++;
-        }
+function printCalendarHeader(year, month) {
+    const weekDays = ['日', '月', '火', '水', '木', '金', '土'];
+    console.log(`${month}月 ${year}`.padStart(13, SPACE));
+    console.log(weekDays.join(SPACE));
 }
+
+function printCalendarDays(year, month) {
+    // 対象年月の1日目の曜日を初期値として取得
+    let dayOfWeek = getDayOfWeek(year, month);
+    // 対象年月の最終日を取得
+    const totalDays = getTotalDays(year, month);
+
+    // １日目の出力場所を調整
+    process.stdout.write(`${SPACE.repeat(3).repeat(dayOfWeek)}`);
+
+    for (let day = 1; day <= totalDays; day++) {
+        let dayString = day.toString().padStart(2, SPACE) + SPACE;
+            if (dayOfWeek == 6) {
+                console.log(dayString);
+                dayOfWeek = 0;
+            } else {
+                process.stdout.write(dayString);
+                dayOfWeek++;
+            }
+    }
+}
+
+function getDayOfWeek(year, month) {
+    return new Date(year, month - 1, 1).getDay();
+}
+
+function getTotalDays(year, month) {
+    return new Date(year, month, 0).getDate();
+}
+
+printCalendarHeader(year, month);
+printCalendarDays(year, month);
